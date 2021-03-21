e215f820c9b4f1af0eed1e712f55a31d850b7ead
TWOJOB <62547528+TWOJOB@users.noreply.github.com>
2021-03-22 01:19:32 +0900

    Update FFRoomCoreBotConfig.java

--- src/main/java/kr/friendlyfire/discord/FFCoreBot/enums/FFRoomCoreBotConfig.java
+++ src/main/java/kr/friendlyfire/discord/FFCoreBot/enums/FFRoomCoreBotConfig.java
@@ -5,23 +5,23 @@ public enum FFRoomCoreBotConfig {
 	/**
 	 * 디스코드 토큰
 	 */
-	TOKEN(""), //디스코드 봇 토큰
+	TOKEN = System.getenv("BOT_TOKEN"), //디스코드 봇 토큰
 	/**
 	 * 서버의 ID
 	 */
-	SERVER_ID(""),
+	SERVER_ID("805958884883038231"),
 	/**
 	 * 만들어진 방의 정보가 출력될 채널의 ID
 	 */
-	PRINT_TEXT_CH_ID(""),
+	PRINT_TEXT_CH_ID("822103582261903430"),
 	/**
 	 * 해당 카테고리(ID기준)에 속한 모든 음성채널이 인스턴스 채널로 만들어짐
 	 */
-	SEARCH_CATEGORY_ID(""),
+	SEARCH_CATEGORY_ID("805958885504057397"),
 	/**
 	 * 이 카테고리에 인스턴스 채널이 생성됨
 	 */
-	MAKE_CATEGORY_ID(""),
+	MAKE_CATEGORY_ID("805958885504057397"),
 	/**
 	 * 봇의 이름
 	 */
